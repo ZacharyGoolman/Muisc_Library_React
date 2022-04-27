51ca86ec9f8a729f5d548c5f84b7484c7c5b19db
ZacharyGoolman <zachary.goolman@gmail.com>
2022-04-27 18:30:36 -0400

    Finished MusicTable component + created CSS for MusicTable

--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,25 +1,42 @@
+import React, { useState } from 'react';
+import './MusicTable.css'
+
+
+
+
 const MusicTable = (props) => {
     return ( 
-        <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Artist</th>
-            <th>Album</th>
-            <th>Release Date</th>
-            <th>Genre</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Id</th>
-            <th>Media</th>
-            <th>Likes</th>
-          </tr>
-        </tbody>
-      </table>
-
-     );
+            <table>
+                 <thead>
+                    <tr>
+                       <th>Artist</th>
+                       <th>Title</th>
+                       <th>Album</th>
+                       <th>Release Date</th>
+                       <th>Genre</th>
+                    </tr>
+                </thead>
+            {props.songs.map((song, index) => {
+                return (
+                <div>
+               
+                <tbody>
+                    <tr>
+                         <td>{index + 1}</td>
+                        <td>{song.artist}</td>
+                        <td>{song.title}</td>
+                        <td>{song.album}</td>
+                        <td>{song.release_date}</td>
+                        <td>{song.genre}</td>
+                        <td><img className="table_image" src={song.media}/></td>
+                        <button>{song.like}</button>
+                    </tr>
+                </tbody>
+                </div>
+                );
+            })}
+            </table>
+    );
 }
  
 export default MusicTable;
